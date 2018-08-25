302691b2b7fe7ff425664991ca4d4d50946424e7
shefy123 <38942966+shefy123@users.noreply.github.com>
2018-08-26 01:28:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function getWeather() {
                 document.querySelector(".main-icon").innerHTML='<i class="wi wi-cloud"></i>';
             }
 
-            else if (icon === `rain`) {
+            else if (icon === `Rain`) {
                 document.querySelector(".main-icon").innerHTML='<i class="wi wi-night-rain-wind"></i>';
             }
             else  {
@@ -50,16 +50,16 @@ function getWeather() {
         console.log(data);
      
         var d1 = new Date(data.list[0].dt*1000); 
-        var d2 = new Date(data.list[6].dt*1000); 
-        var d3 = new Date(data.list[14].dt*1000); 
-        var d4 = new Date(data.list[26].dt*1000); 
-        var d5 = new Date(data.list[30].dt*1000); 
+        var d2 = new Date(data.list[8].dt*1000); 
+        var d3 = new Date(data.list[16].dt*1000); 
+        var d4 = new Date(data.list[24].dt*1000); 
+        var d5 = new Date(data.list[32].dt*1000); 
  
-        // document.querySelector(".day-one").innerHTML = days[d1.getDay()]; 
-        // document.querySelector(".day-two").innerHTML = days[d2.getDay()];        
-        // document.querySelector(".day-three").innerHTML = days[d3.getDay()];        
-        // document.querySelector(".day-four").innerHTML = days[d4.getDay()];  
-        // document.querySelector(".day-five").innerHTML = days[d5.getDay()]; 
+        document.querySelector(".day1").innerHTML = days[d1.getDay()]; 
+        document.querySelector(".day2").innerHTML = days[d2.getDay()];        
+        document.querySelector(".day3").innerHTML = days[d3.getDay()];        
+        document.querySelector(".day4").innerHTML = days[d4.getDay()];  
+        document.querySelector(".day5").innerHTML = days[d5.getDay()]; 
 
         document.querySelector(".date-one").innerHTML = `${months[d1.getMonth()]} ${d1.getDate()}`;        
         document.querySelector(".date-two").innerHTML = `${months[d2.getMonth()]} ${d2.getDate()}` ;        
@@ -68,24 +68,24 @@ function getWeather() {
         document.querySelector(".date-five").innerHTML = `${months[d5.getMonth()]} ${d5.getDate()}`;        
 
         document.querySelector(".temperature-one").innerHTML = Math.round(data.list[0].main.temp) + " C°";        
-        document.querySelector(".temperature-two").innerHTML = Math.round(data.list[6].main.temp) + " C°";          
-        document.querySelector(".temperature-three").innerHTML = Math.round(data.list[14].main.temp) + " C°";         
-        document.querySelector(".temperature-four").innerHTML = Math.round(data.list[26].main.temp) + " C°";         
-        document.querySelector(".temperature-five").innerHTML = Math.round(data.list[30].main.temp) + " C°";         
+        document.querySelector(".temperature-two").innerHTML = Math.round(data.list[5].main.temp) + " C°";          
+        document.querySelector(".temperature-three").innerHTML = Math.round(data.list[13].main.temp) + " C°";         
+        document.querySelector(".temperature-four").innerHTML = Math.round(data.list[21].main.temp) + " C°";         
+        document.querySelector(".temperature-five").innerHTML = Math.round(data.list[29].main.temp) + " C°";         
     
-        let moreIcon = data.list[0].weather[0].main;
+        // let moreIcon = data.list[0].weather[0].main;
 
         
-        if (moreIcon === `smoke`) {
-            document.querySelector("#icon").innerHTML='<i class="wi wi-smoke"></i>';
-            console.log ("smoke");
-        }
-        else if (moreIcon === `Clouds`) {
-            document.querySelector("#icon").innerHTML='<i class="wi wi-cloud"></i>';
-        }
-        else  {
-            document.querySelector("#icon").innerHTML='no icon match';
-        }
+        // if (moreIcon === `smoke`) {
+        //     document.querySelector("#icon").innerHTML='<i class="wi wi-smoke"></i>';
+        //     console.log ("smoke");
+        // }
+        // else if (moreIcon === `Clouds`) {
+        //     document.querySelector("#icon").innerHTML='<i class="wi wi-cloud"></i>';
+        // }
+        // else  {
+        //     document.querySelector("#icon").innerHTML='no icon match';
+        // }
 
     },
     error: function(error){
